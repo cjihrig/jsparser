d0a041744313b2497b63262e88cdb37d6da5c016
Jacobo221 <jacobo221@gmail.com>
2014-06-06 03:04:06 +0200

    Applied coding style to previous patch ("Do not include all brackets")

--- print.js
+++ print.js
@@ -1,45 +1,50 @@
 "use strict";
 
+var precedence = [  // from less to most preferent
+	[ "=", "+=", "-=", "*=", "/=", "%=", "<<=", ">>=", ">>>=", "&=", "^=", "|=" ],
+	"||",
+	"&&",
+	"|",
+	"^",
+	"&",
+	[ "<", "<=", ">", ">=", "in", "instanceof" ],
+	[ "<<", ">>", ">>>" ],
+	[ "+", "-" ],
+	[ "*", "/", "%" ],
+	[ "!", "~", "+unary", "-unary", "typeof", "void", "delete" ],
+	[ "++", "--" ]
+];
+
 (function(parser) {
 	var ast = parser.ast;
 
-	function needBrackets(firstToken,secondToken) {
+	function needBrackets(firstToken, secondToken) {
 		var firstOp = firstToken.operator;
-		if ((firstToken.type == "UnaryExpression") &&
-		    (firstToken.operator == "-" || firstToken.operator == "+")) {
+		if (firstToken.type === "UnaryExpression" &&
+		    (firstToken.operator === "-" || firstToken.operator === "+")) {
 			 firstOp += "unary";
 		}
 		var secondOp = secondToken.operator;
-		if ((secondToken.type == "UnaryExpression") &&
-		    (secondToken.operator == "-" || secondToken.operator == "+")) {
+		if (secondToken.type == "UnaryExpression" &&
+		    (secondToken.operator === "-" || secondToken.operator === "+")) {
 			 secondOp += "unary";
 		}
-		var precedence = new Array(  // from less to most preferent
-			new Array("=","+=","-=","*=","/=","%=","<<=",">>=",">>>=","&=","^=","|="),
-			"||",
-			"&&",
-			"|",
-			"^",
-			"&",
-			new Array("<","<=",">",">=","in","instanceof"),
-			new Array("<<",">>",">>>"),
-			new Array("+","-"),
-			new Array("*","/","%"),
-			new Array("!","~","+unary","-unary","typeof","void","delete"),
-			new Array("++","--")
-		);
-		var firstPos = precedence.length, secondPos = precedence.length;
-		for (var i=0;i<precedence.length;i++) {
-			for (var j=0;j<precedence[i].length;j++) {
-				if (firstOp === precedence[i][j]) {
+		var firstPos = precedence.length;
+		var secondPos = precedence.length;
+		for (var i = 0; i < precedence.length; i++) {
+			for (var j = 0; j < precedence[i].length; j++) {
+				var operation = precedence[i][j];
+				if (firstOp === operation) {
 					firstPos = i;
 				}
-				if (secondOp === precedence[i][j]) {
+
+				if (secondOp === operation) {
 					secondPos = i;
 				}
 			}
 		}
-		return (firstPos > secondPos);
+
+		return firstPos > secondPos;
 	}
 
 	ast.ProgramNode.prototype.print = function(indent, indentChar) {
@@ -434,62 +439,75 @@
 		} else if (needBrackets(this, this.argument)) {
 			return operator + "(" + this.argument.print("", "") + ")";
 		} else {
-			return operator + this.argument.print("", "")
+			return operator + this.argument.print("", "");
 		}
 	};
 
 	ast.BinaryExpressionNode.prototype.print = function(indent, indentChar) {
 		var str = "";
-		if (needBrackets(this,this.left)) {
+
+		if (needBrackets(this, this.left)) {
 			str += "(" + this.left.print("", "") + ")";
 		} else {
 			str += this.left.print("", "");
 		}
+
 		str += " " + this.operator + " ";
-		if (needBrackets(this,this.right)) {
+
+		if (needBrackets(this, this.right)) {
 			str += "(" + this.right.print("", "") + ")";
 		} else {
 			str += this.right.print("", "");
 		}
+
 		return str;
 	};
 
 	ast.AssignmentExpressionNode.prototype.print = function(indent, indentChar) {
 		var str = this.left.print("", "") + " " + this.operator + " ";
-		if (needBrackets(this,this.right)) {
+
+		if (needBrackets(this, this.right)) {
 			str += "(" + this.right.print("", "") + ")";
 		} else {
 			str += this.right.print("", "");
 		}
+
 		return str;
 	};
 
 	ast.UpdateExpressionNode.prototype.print = function(indent, indentChar) {
 		var str = this.argument.print("", "");
+
 		if (needBrackets(this, this.argument)) {
 			str = "(" + str + ")";
 		}
+
 		if (this.prefix) {
 			str = this.operator + str;
 		} else {
 			str = str + this.operator;
 		}
+
 		return str;
 	};
 
 	ast.LogicalExpressionNode.prototype.print = function(indent, indentChar) {
 		var str = "";
-		if (needBrackets(this,this.left)) {
+
+		if (needBrackets(this, this.left)) {
 			str += "(" + this.left.print("", "") + ")";
 		} else {
 			str += this.left.print("", "");
 		}
+
 		str += " " + this.operator + " ";
-		if (needBrackets(this,this.right)) {
+
+		if (needBrackets(this, this.right)) {
 			str += "(" + this.right.print("", "") + ")";
 		} else {
 			str += this.right.print("", "");
 		}
+
 		return str;
 	};
 
@@ -573,3 +591,4 @@
 		return this.value;
 	};
 })(ecmascript);
+
